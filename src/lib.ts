5b42379466ed6635524c50d53ae075ba1145607a
Alexander "dotKuro" Kampf <mail@akampf.dev>
2021-11-16 20:50:30 +0100

    feat: Add entrypoint

---
+++ src/lib.ts
@@ -0,0 +1,22 @@
+import { ComponentThemeFactory } from './ComponentThemeFactory';
+import { initializeSettingsContext } from './initializeSettingsContext/initializeSettingsContext';
+import { SettingsCollection } from './SettingsCollection';
+import { SettingsContext } from './SettingsContext';
+import { UseComponentTheme } from './UseComponentTheme';
+import { UseSettings } from './UseSettings';
+import { UseSettingsCollection } from './UseSettingsCollection';
+import { UseThemingVariant } from './UseThemingVariant';
+
+export {
+  initializeSettingsContext
+};
+
+export type {
+  ComponentThemeFactory,
+  SettingsCollection,
+  SettingsContext,
+  UseComponentTheme,
+  UseSettings,
+  UseSettingsCollection,
+  UseThemingVariant
+};
